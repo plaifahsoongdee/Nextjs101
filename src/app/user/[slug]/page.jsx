87cc3e1a2d1afa0dbe1f3ce519fef72231dcd139
plaifahsoongdee <plaifahsoongdee@gmail.com>
2024-10-05 20:08:24 +0700

    nextjs

---
+++ src/app/user/[slug]/page.jsx
@@ -0,0 +1,12 @@
+export default function Page({ params }) {
+  return(
+    <>
+    <h1 className="text-cyan-500">Hello, Next.js User ...{params.slug}
+
+    </h1>
+    
+    </>
+  );
+  
+ 
+}
---
+++ src/app/user/page.jsx
@@ -0,0 +1,15 @@
+export default function Page() {
+    return (
+      <> 
+        <h1>Hello, Next.js! user</h1>
+        <ul>
+          <li>User no. 1</li>
+          <li>User no. 2</li>
+          <li>User no. 3</li>
+          <li>User no. 4</li>
+          <li>User no. 5</li>
+        </ul>
+      </>
+    );
+  }
+  
